d8bf5b45bb9474bd3f67cfa2d979b4ab83ca718c
justin3go <justin3go@qq.com>
2023-06-17 16:34:05 +0800

    feat: 添加rss订阅

--- docs/.vitepress/config.ts
+++ docs/.vitepress/config.ts
@@ -1,5 +1,6 @@
 import { defineConfig } from "vitepress";
 import { createSidebar } from "./utils/createSidebar";
+import { createRssFile } from "./utils/rss";
 
 // https://vitepress.dev/reference/site-config
 export default defineConfig({
@@ -7,7 +8,18 @@ export default defineConfig({
 	description: "坚持深耕技术领域的T型前端程序员, 喜欢Vuejs、Nestjs, 还会点python、nlp、web3、后端",
 	lang: "zh-CH", //语言
 	lastUpdated: true,
-	lastUpdatedText: '最近更新',
+	lastUpdatedText: "最近更新",
+	head: [
+		// 改变title的图标
+		[
+			"link",
+			{
+				rel: "icon",
+				href: "https://oss.justin3go.com/justin3goAvatar.ico",
+			},
+		],
+	],
+	buildEnd: createRssFile,
 	themeConfig: {
 		// https://vitepress.dev/reference/default-theme-config
 		outline: [2, 4],
@@ -41,6 +53,12 @@ export default defineConfig({
 		socialLinks: [
 			{ icon: "github", link: "https://github.com/Justin3go/justin3go.github.io" },
 			{ icon: "twitter", link: "https://twitter.com/_Justin3go" },
+			{
+        icon: {
+          svg: '<svg xmlns="http://www.w3.org/2000/svg" width="512" height="512" viewBox="0 0 512 512"><title>RSS订阅</title><path d="M108.56,342.78a60.34,60.34,0,1,0,60.56,60.44A60.63,60.63,0,0,0,108.56,342.78Z"/><path d="M48,186.67v86.55c52,0,101.94,15.39,138.67,52.11s52,86.56,52,138.67h86.66C325.33,312.44,199.67,186.67,48,186.67Z"/><path d="M48,48v86.56c185.25,0,329.22,144.08,329.22,329.44H464C464,234.66,277.67,48,48,48Z"/></svg>'
+        },
+        link: '/feed.xml'
+      }
 		],
 
 		footer: {
@@ -49,18 +67,17 @@ export default defineConfig({
 				"&nbsp &nbsp | &nbsp &nbsp" +
 				'<a href="https://oss.justin3go.com/blogs/Justin3goWechat.jpg" target="_blank">联系笔者微信(注意备注来意)</a>',
 			copyright:
-				'<a href="https://beian.miit.gov.cn/#/Integrated/index" target="_blank">Copyright© 2021-2023 渝ICP备2021006879号</a>',
+				'<a href="https://beian.miit.gov.cn/#/Integrated/index" target="_blank">Copyright© 2021-present 渝ICP备2021006879号</a>',
 		},
 		search: {
 			provider: "local",
 		},
 		editLink: {
-      pattern: 'https://github.com/Justin3go/justin3go.github.io/edit/master/docs/:path',
-			text: '在GitHub上编辑此页',
-    },
-		returnToTopLabel: '👆Code is building the world.',
-		sidebarMenuLabel: '目录',
-		darkModeSwitchLabel: '白/夜'
+			pattern: "https://github.com/Justin3go/justin3go.github.io/edit/master/docs/:path",
+			text: "在GitHub上编辑此页",
+		},
+		returnToTopLabel: "👆Code is building the world.",
+		sidebarMenuLabel: "目录",
+		darkModeSwitchLabel: "白/夜",
 	},
 });
-
---
+++ docs/.vitepress/utils/rss.ts
@@ -0,0 +1,58 @@
+import path from "path";
+import { writeFileSync } from "fs";
+import { Feed } from "feed";
+import { createContentLoader, type SiteConfig } from "vitepress";
+
+const hostname = "https://justin3go.com";
+
+export async function createRssFile(config: SiteConfig) {
+	const feed = new Feed({
+		title: "Justin3go's Blog-🖊",
+		description: "坚持深耕技术领域的T型前端程序员, 喜欢Vuejs、Nestjs, 还会点python、nlp、web3、后端",
+		id: hostname,
+		link: hostname,
+		language: "zh-CH",
+		image: "https://oss.justin3go.com/justin3goAvatar.png",
+		favicon: `https://oss.justin3go.com/justin3goAvatar.ico`,
+		copyright: "Copyright© 2021-present 渝ICP备2021006879号",
+	});
+
+	const posts = await createContentLoader("博客/**/*.md", {
+		excerpt: true,
+		render: true,
+	}).load();
+
+	posts.sort((a, b) => Number(+getDate(b.url) - +getDate(a.url)));
+
+	for (const { url, excerpt, frontmatter, html } of posts) {
+    // 排除index.md与2022|2021|2020发布的文章
+    if(url.search(/index|202[0-2]/) >= 0) {
+      continue;
+    }
+    // 仅保留最近3篇文章
+    if(feed.items.length >= 3) {
+      break;
+    } 
+		feed.addItem({
+			title: frontmatter.title,
+			id: `${hostname}${url}`,
+			link: `${hostname}${url}`,
+			description: excerpt,
+			content: html,
+			author: [
+				{
+					name: "Justin3go",
+					email: "justin3go@qq.com",
+					link: "https://justin3go.com",
+				},
+			],
+			date: getDate(url),
+		});
+	}
+
+	writeFileSync(path.join(config.outDir, "feed.xml"), feed.rss2(), "utf-8");
+}
+
+export function getDate(url: string) {
+	return new Date(url.substring(4, 14));
+}
