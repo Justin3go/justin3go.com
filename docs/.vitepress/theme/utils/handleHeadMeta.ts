f8f0353a7e0e70d82b07f91e924e98eb86a20b6e
justin3go <just@justin3go.com>
2024-07-30 22:12:46 +0800

    chore: 更新封面图片链接

--- docs/.vitepress/theme/utils/handleHeadMeta.ts
+++ docs/.vitepress/theme/utils/handleHeadMeta.ts
@@ -5,7 +5,7 @@ export function handleHeadMeta(context: TransformContext) {
   const { description, title, relativePath, frontmatter } = context.pageData;
 
   const curDesc = description || context.description;
-  const cover = frontmatter.cover || 'https://justin3go.com/ava.jpg'
+  const cover = frontmatter.cover || 'https://justin3go.com/bg.jpg'
   const cardType = frontmatter.cover ? 'summary_large_image' : 'summary'
   // 增加Twitter卡片
   const ogUrl: HeadConfig = ["meta", { property: "og:url", content: addBase(relativePath) }]
